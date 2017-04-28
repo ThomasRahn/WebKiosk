257e70b230ad66afe98b9b41849cb504d2974fde
Thomas Rahn <thomas@rahn.ca>
2017-04-28 13:05:07 -0400

    Updated Android manifest for bootservice

--- WebKiosk/app/src/main/java/com/example/thomas/webkiosk/MainActivity.java
+++ WebKiosk/app/src/main/java/com/example/thomas/webkiosk/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.thomas.webkiosk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webv = (WebView) findViewById(R.id.webview);
-        webv.loadUrl("http://www.example.com");
+        webv.loadUrl("https://www.sednove.com");
         webv.setWebViewClient(new WebViewClient());
         WebSettings webSettings = webv.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -24,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed(){
         Toast.makeText(getApplicationContext(),"You Are Not Allowed to Exit the App", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        /*if (keyCode == KeyEvent.KEYCODE_HOME) {
+            Log.i("TEST", "Home Button");  // here you'll have to do something to prevent the button to go to the home screen
+            return true;
+        }*/
+
+        /*if(keyCode == KeyEvent.KEYCODE_APP_SWITCH){
+
+        }*/
+        return super.onKeyDown(keyCode, event);
     }
 }
