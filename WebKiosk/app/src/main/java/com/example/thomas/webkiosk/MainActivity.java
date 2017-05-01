ffcb881d3779eb47de31e32d30611b1d16e36f06
unknown <thomas@rahn.ca>
2017-05-01 18:47:17 -0400

    Fixed gradle problems. Updated code to block home/recent app buttons

--- WebKiosk/app/src/main/java/com/example/thomas/webkiosk/MainActivity.java
+++ WebKiosk/app/src/main/java/com/example/thomas/webkiosk/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.thomas.webkiosk;
 
+import android.content.DialogInterface;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.app.AlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String m_Text = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
 
         WebView webv = (WebView) findViewById(R.id.webview);
-        webv.loadUrl("https://www.sednove.com");
+
         webv.setWebViewClient(new WebViewClient());
         WebSettings webSettings = webv.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
+
+
+        webv.loadUrl("http://www.sednove.com");
     }
 
     @Override
@@ -28,15 +43,41 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),"You Are Not Allowed to Exit the App", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        /*if (keyCode == KeyEvent.KEYCODE_HOME) {
-            Log.i("TEST", "Home Button");  // here you'll have to do something to prevent the button to go to the home screen
-            return true;
-        }*/
 
-        /*if(keyCode == KeyEvent.KEYCODE_APP_SWITCH){
+        if (keyCode == KeyEvent.KEYCODE_HOME || keyCode == KeyEvent.KEYCODE_APP_SWITCH) {
 
-        }*/
-        return super.onKeyDown(keyCode, event);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Title");
+
+            // Set up the input
+            final EditText input = new EditText(this);
+            // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+            input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            builder.setView(input);
+
+
+            // Set up the buttons
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    m_Text = input.getText().toString();
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            builder.show();
+            Toast.makeText(getApplicationContext(),"You Are Not Allowed to Exit the App", Toast.LENGTH_SHORT).show();
+            Log.i("Home Button","Clicked");
+            return false;
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
     }
 }
